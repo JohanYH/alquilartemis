e85e6aa3d5ea98e8e9855db5e23333d13fae5338
JohanYH <johanpuerto2005@gmail.com>
2023-06-29 13:32:28 -0500

    METODOS HTTP

--- backend/src/controllers/categoria.controllers.js
+++ backend/src/controllers/categoria.controllers.js
@@ -1,9 +1,11 @@
 import getConectar from "./../db/database.js";
 
+
+//1. Esta funcion llama a todo la informacion de la tabla
 const getCategorias = async (req, res) => {
     try {
         const categoria = await getConectar();
-        const result = await categoria.query("SELECT id_categoria, nombre_categoria, descripcion_categoria, img_categoria FROM categorias");
+        const result = await categoria.query("SELECT * FROM categorias");
         console.log(result);
         res.json(result); 
     } catch (error) {
@@ -12,11 +14,12 @@ const getCategorias = async (req, res) => {
     }
 }
 
+//2. Esta funcion muestra la informacion de la tabla, pero atravez de una ID especifico
 const getCategoria = async (req, res) => {
     try {
      const {id} = req.params;
      const connection = await getConectar();
-     const result = await connection.query("SELECT id_categoria,nombre_categoria, descripcion_categoria,img_categoria FROM categorias WHERE id_categoria =?", id);
+     const result = await connection.query("SELECT * FROM categorias WHERE id_categoria =?", id);
      res.json(result);   
     } catch (error) {
         res.status(500);
@@ -24,6 +27,8 @@ const getCategoria = async (req, res) => {
     }
 }
 
+
+//3. Esta funcion añade datos a la tabla
 const addCategorias = async (req, res) => {
     try {
         const {nombre_categoria,descripcion_categoria,img_categoria} = req.body;
@@ -37,6 +42,8 @@ const addCategorias = async (req, res) => {
     }
 }
 
+
+//4. Esta funcion borra datos de la tabla, pero atravez de un ID especifico
 const deleteCategorias = async (req, res) =>{
     try {
         const {id} = req.params;
@@ -49,6 +56,8 @@ const deleteCategorias = async (req, res) =>{
     }
 }
 
+
+//5.Esta funcion modifica la informacion que contiene un dato de una tabla
 const updateCategoria = async (req,res) =>{
     try {
         const {id} =req.params
@@ -66,7 +75,7 @@ const updateCategoria = async (req,res) =>{
 }
 
 
-
+//6. Este metodo permite exportar un objeto las funciones  
 export const  methodsHTTPS = {
     getCategorias,
     addCategorias,
--- backend/src/routes/categorias.routes.js
+++ backend/src/routes/categorias.routes.js
@@ -1,10 +1,15 @@
-import {Router} from "express";
+import { Router } from "express";
 
+//1. Importamos el metodo HTTP que esta en al carpeta Controllers y esta funcion sirve para llamar en las rutas
 import { methodsHTTPS as categoriasController } from "../controllers/categoria.controllers.js";
 const router = Router();
 
+//2.El router se encarga de indentificar la ruta en la que se encuetra nuestro navegador
+//2.1. El metodo Get llama a toda la base de datos y El metodo POST añade nueva base de datos
 router.get("/", categoriasController.getCategorias);
-router.post("/",categoriasController.addCategorias);
+router.post("/", categoriasController.addCategorias);
+
+//2.2. El metodo Get con Id indentifica solo el id, el metodo Delete elimina con la Id Y El metodo PUT es como un Update que cambia la base de datos
 router.get("/:id", categoriasController.getCategoria);
 router.delete("/:id", categoriasController.deleteCategorias);
 router.put("/:id", categoriasController.updateCategoria);
